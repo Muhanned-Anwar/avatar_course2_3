f3c4772d718a38e554c8bffe583571f7cf2eac0d
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-24 17:05:00 +0300

    create AboutScreen

---
+++ lib/about_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
