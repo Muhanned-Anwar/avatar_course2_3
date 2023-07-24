ffaa03cc8b6c9cf6b650a184f20f419f2b80fa21
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-24 17:05:32 +0300

    wdit splash

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,11 @@
+import 'package:avatar_course2_3/about_screen.dart';
+import 'package:avatar_course2_3/core/resources/manager_font_sizes.dart';
+import 'package:avatar_course2_3/core/resources/manager_font_weight.dart';
+import 'package:avatar_course2_3/core/resources/manager_strings.dart';
 import 'package:avatar_course2_3/core/routes.dart';
 import 'package:flutter/material.dart';
+
+import 'bio_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -16,22 +22,40 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(Duration(seconds: 3), () {
       print('Hello I am in Future');
       // Navigator.pushNamed(context, Routes.bioScreen);
-      Navigator.pushReplacementNamed(context, Routes.bioScreen);
+      // Navigator.pushReplacementNamed(context, Routes.bioScreen);
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: (context) => const AboutScreen(),
+      //   ),
+      // );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AboutScreen(),
+        ),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text(
-          'Bio App',
+          ManagerStrings.bioApp,
           style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+            fontSize: ManagerFontSizes.s24,
+            fontWeight: ManagerFontWeight.w600,
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    print('Hello i am in dispose');
+    super.dispose();
   }
 }
