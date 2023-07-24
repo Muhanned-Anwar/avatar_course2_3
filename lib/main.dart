a6e0082ecfe245b2e7d15ad5f48ed8872f35584d
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-24 18:18:33 +0300

    add theme and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:avatar_course2_3/about_screen.dart';
 import 'package:avatar_course2_3/bio_screen.dart';
+import 'package:avatar_course2_3/core/resources/manager_font_sizes.dart';
 import 'package:avatar_course2_3/core/routes.dart';
 import 'package:avatar_course2_3/splash_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'core/constants.dart';
+import 'core/resources/manager_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +19,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: ManagerFontSizes.s20,
+            color: Colors.black,
+          ),
+        ),
+      ),
       initialRoute: Routes.splashScreen,
       routes: {
         Routes.splashScreen: (context) => const SplashScreen(),
         Routes.bioScreen: (context) => const BioScreen(),
+        Routes.aboutScreen: (context) => const AboutScreen(),
       },
     );
   }
