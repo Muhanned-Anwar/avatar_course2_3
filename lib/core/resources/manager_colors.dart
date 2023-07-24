98d1ae4198a1e10f4babc14c64b2dd7ed3570cdf
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-24 18:18:40 +0300

    create ManagerColors

---
+++ lib/core/resources/manager_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static Color primaryColor = Colors.blue.shade400;
+  static Color secondaryColor = Colors.blue.shade700;
+  static Color transparent = Colors.transparent;
+  static Color white = Colors.white;
+}
