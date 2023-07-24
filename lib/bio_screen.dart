501c3e05c73bd36119e2ac85cf53578fe2d4ca4f
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-24 18:17:56 +0300

    Add content to BioScreen

--- lib/bio_screen.dart
+++ lib/bio_screen.dart
@@ -1,3 +1,8 @@
+import 'package:avatar_course2_3/core/constants.dart';
+import 'package:avatar_course2_3/core/resources/manager_colors.dart';
+import 'package:avatar_course2_3/core/resources/manager_font_sizes.dart';
+import 'package:avatar_course2_3/core/resources/manager_strings.dart';
+import 'package:avatar_course2_3/core/routes.dart';
 import 'package:flutter/material.dart';
 
 class BioScreen extends StatelessWidget {
@@ -6,7 +11,40 @@ class BioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.red,),
+      appBar: AppBar(
+        title: Text(
+          ManagerStrings.bioApp,
+          style: TextStyle(
+            fontSize: ManagerFontSizes.s24,
+            color: ManagerColors.white,
+          ),
+        ),
+        backgroundColor: ManagerColors.transparent,
+        elevation: Constants.appBarElevation,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, Routes.aboutScreen);
+            },
+            icon: const Icon(
+              Icons.info_outline,
+            ),
+          )
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: AlignmentDirectional.topStart,
+            end: AlignmentDirectional.bottomEnd,
+            colors: [
+              ManagerColors.primaryColor,
+              ManagerColors.secondaryColor,
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
