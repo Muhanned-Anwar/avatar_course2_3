6b666397423c86a8be72cbd218045d8d24bef728
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-29 15:47:47 +0300

    change color

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -3,8 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ManagerColors {
-  static Color primaryColor = Colors.blue.shade400;
-  static Color secondaryColor = Colors.blue.shade700;
+  static Color primaryColor = Colors.blue.shade700;
+  static Color secondaryColor = Colors.blue.shade200;
   static Color transparent = Colors.transparent;
   static Color white = Colors.white;
 }
